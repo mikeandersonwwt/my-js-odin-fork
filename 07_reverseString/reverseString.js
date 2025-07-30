70372898d7b53c3ce286bdfabbce5803ea6f9c69
Mike <mike.anderson@wwt.com>
2025-07-30 15:05:44 -0500

    adding tests

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(text) {
 
+    let result = text.split("").reverse().join("");
+
+    return result;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+function removeFromArray(arr, ...args) {
+    const newArr = [];
+    arr.forEach((value) => {
+        if(!args.includes(value)) {
+            newArr.push(value);
+        }
+    });
+    return newArr;
 };
+
+// The below also works using filter()
+// function removeFromArray(arr, ...valuesToRemove) {
+//     return arr.filter(value => !valuesToRemove.includes(value));
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
