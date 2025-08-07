7d87631da5869d796a7bb62c6d5096fc356a157b
Mike <mike.anderson@wwt.com>
2025-08-07 09:55:08 -0500

    new exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    const min = Math.min(int1, int2);
+    const max = Math.max(int1, int2);
+    let sum = 0;
 
+    if (int1 < 0 || int2 < 0 ||
+        typeof int1 !== "number" ||
+        typeof int2 !== "number" ||
+        !Number.isInteger(int1) ||
+        !Number.isInteger(int2)
+    ) {
+        return "ERROR";
+    } 
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+
+// Implement a function that takes 2 positive integers and returns the sum of 
+// every integer between (and including) them:
+
+// make sure you pay attention to the function parameters
+// create a variable to hold the final sum
+// loop through the given numbers (link)
+// on each iteration add the number to the sum
+// return the sum after finishing the loop
--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // const isYearDivisibleByFour = year % 4 === 0;
+    // const isCentury = year % 100 === 0;
+    // const isYearDivisibleByFourHundred = year % 400 === 0;
+
+    // if (
+    //     isYearDivisibleByFour &&
+    //     (!isCentury || isYearDivisibleByFourHundred)
+    // ) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    if (year % 4 === 0 && 
+        (year % 400 === 0 || year % 100 !== 0)) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
